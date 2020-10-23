5ad4b876938e616a78db811d6a5667e7e377f855
Insiyaa <insiyahhajoori@gmail.com>
2020-10-23 21:57:27 +0530

    Add Nav Bar

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+
+function DefaultScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Default!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            switch (route.name) {
+              case 'Feed':
+                iconName = 'md-home';
+                break;
+              case 'Search':
+                iconName = 'ios-search';
+                break;
+              case 'Add':
+                iconName = 'ios-add-circle';
+                break;
+              case 'Notifications':
+                iconName = 'ios-notifications';
+                break;
+              case 'Profile':
+                iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+                break;
+            
+              default:
+                break;
+            }
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: '#5E5757',
+        }}
+      >
+        <Tab.Screen name="Feed" component={DefaultScreen} />
+        <Tab.Screen name="Search" component={DefaultScreen} />
+        <Tab.Screen name="Add" component={DefaultScreen} />
+        <Tab.Screen name="Notifications" component={DefaultScreen} />
+        <Tab.Screen name="Profile" component={DefaultScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
