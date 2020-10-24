6551be8b42bce91f31300df476a8afdebb7877a0
Insiyaa <insiyahhajoori@gmail.com>
2020-10-25 00:21:19 +0530

    Patch react-native-card-animated-modal

--- components/eventScreenComponents/EventCardList.js
+++ components/eventScreenComponents/EventCardList.js
@@ -62,7 +62,7 @@ export default class Vertical extends React.Component {
         renderDetails={() => (
           <View
             style={{
-              paddingVertical: 30,
+              paddingVertical: 36,
               paddingHorizontal: 16
             }}
           >
@@ -70,20 +70,9 @@ export default class Vertical extends React.Component {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              nisi ut aliquip ex ea commodo consequat. 
             </Text>
-						<Text style={{ color: "rgba(0, 0, 0, 0.7)", fontSize: 18 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Text>
+
           </View>
         )}
       />
