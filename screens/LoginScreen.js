158ce68b0283ce71aebb1f6f50cb20d8a64fdd3d
sayoojbk <sayoojbk@gmail.com>
2020-10-29 16:49:04 +0530

    Updated Login Screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,23 +1,108 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity,SafeAreaView } from 'react-native';
 
-import LoginScreen from "react-native-login-screen";
-
-export default function App() {
+export default class LoginScreen extends React.Component {
+  state={
+    email:"",
+    password:""
+  }
+  // AppNavigator = StackNavigator({
+  //   Cluster1: { screen: Cluster1},
+  //    Play: { screen: Play},
+  //  });
+   
+  handleOnPress = () => {
+    console.log('user login pressed')
+  }
+  render(){
     return (
       <SafeAreaView style={styles.container}>
-      {/* // <SafeAreaView> */}
-        <LoginScreen />
+        <Text style={styles.logo}>Fripple</Text>
+        <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Email..." 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput  
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password..." 
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({password:text})}/>
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.handleOnPress} style={styles.loginBtn}>
+          <Text style={styles.loginText}>User Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginBtn2}>
+          <Text style={styles.loginText}>Company Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.loginText}>Signup</Text>
+        </TouchableOpacity>
+
+  
       </SafeAreaView>
     );
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      height:'100%',
-      backgroundColor: '#FDFDFD',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#003f5c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  forgot:{
+    color:"white",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:20
+  },
+  loginBtn2:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    // marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
+  }
+});
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
 
+import LoginScreen from '../screens/LoginScreen';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-    </SafeAreaView>
+    // <SafeAreaView style={styles.container}>
+      <LoginScreen/>
+    // </SafeAreaView>
   );
 }
 
